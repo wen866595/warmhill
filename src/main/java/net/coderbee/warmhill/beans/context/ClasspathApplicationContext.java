0965dd6c9c852bcafaa0a9dc9c1d429b205ae914
coderbee <wen866595@163.com>
2017-12-24 11:12:57 +0800

    添加注释。。

--- src/main/java/net/coderbee/warmhill/beans/context/ClasspathApplicationContext.java
+++ src/main/java/net/coderbee/warmhill/beans/context/ClasspathApplicationContext.java
@@ -8,6 +8,10 @@ import net.coderbee.warmhill.beans.io.XmlBeanDefinitionReader;
 import java.util.List;
 
 /**
+ * 应用上下文的两大核心职责：<br/>
+ * 1. 从给定资源加载 bean 定义。<br/>
+ * 2. 用 bean 定义构建 bean 容器工厂。
+ *
  * @author coderbee on 2017/12/24.
  */
 public class ClasspathApplicationContext extends AbstractApplicationContext {
